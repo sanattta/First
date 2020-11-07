c6f5c20fd95f84482203fc7714b16612eae17935
sanattta <sanata-mail@mail.ru>
2020-11-07 16:15:44 +0300

    lesson7.2 lesson8.4

---
+++ src/com/sanattta/lesson8/task4/entities/Validatable.java
@@ -0,0 +1,7 @@
+package com.sanattta.lesson8.task4.entities;
+
+public interface Validatable {
+
+    void validate();
+
+}
---
+++ src/com/sanattta/lesson8/task4/validator/ValidatorStub.java
@@ -0,0 +1,31 @@
+package com.sanattta.lesson8.task4.validator;
+
+
+import com.sanattta.lesson8.task4.entities.Entity;
+
+import java.util.HashSet;
+import java.util.Set;
+import java.util.UUID;
+
+public class ValidatorStub {
+    private static final Set<Entity> entities = new HashSet<>();
+
+    public static void validateEntity(Entity entity) {
+        System.out.printf("Сущность до валидации: %s%n", entity);
+        if (entities.contains(entity)) {
+            for (Entity stubEntity : entities) {
+                if (stubEntity.equals(entity)) {
+                    String uuid = stubEntity.getUuid();
+                    entity.setUuid(uuid);
+                    break;
+                }
+            }
+        } else {
+            String uuid = UUID.randomUUID().toString();
+            entity.setUuid(uuid);
+            entities.add(entity);
+        }
+        System.out.printf("Сущность после валидации: %s%n", entity);
+    }
+
+}
